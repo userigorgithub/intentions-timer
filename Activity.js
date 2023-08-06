f42f9255a369f847b9ed933c17c3c822ea6a4944
Igor Decess <i.decess@comcast.net>
2023-08-06 17:53:19 -0600

    Refactor activity class to include timer method that is functional

--- Activity.js
+++ Activity.js
@@ -11,4 +11,36 @@ class Activity {
   markComplete() {
     this.completed = true;
   }
+
+  beginTimer(minutes, seconds) {
+    var timer = setInterval(function() {
+      if (seconds > 0) {
+        seconds -= 1;
+      } else if (minutes > 0) {
+        minutes -= 1;
+        seconds = 59;
+      } else {
+        clearInterval(timer);
+        timerButton.innerText = "COMPLETE!";
+        logCurrentActivity.classList.remove('hidden');
+      }
+      minutes = minutes.toString().padStart(2, "0");
+      seconds = seconds.toString().padStart(2, "0");
+      startTime.innerText = `${minutes}:${seconds}`}, 1000)
+
+  //   if (secs.innerText != 0) {
+  //     "0" + secs.innerText--;
+  //   } else if (mins.innerText != 0 && secs.innerText == 0) {
+  //     secs.innerText = 59;
+  //     mins.innerText--;
+  //   } else if (mins.innerText && secs.innerText === 0) {
+  //     logCurrentActivity.classList.remove('.hidden');
+  //   } else if (mins.innerText && secs.innerText == 0) {
+  //     clearInterval(upTimer);
+  //     timerButton.innerText = "COMPLETE!";
+  //     currentActivity = new Activity()
+  //     currentActivity.markComplete();
+  //     logCurrentActivity.classList.remove('hidden');
+  //   }
+  }
 }
